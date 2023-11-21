eb88b1c18e0e964e6c935c872d00aca453dbd32a
andr3alobascio <101721027+andr3alobascio@users.noreply.github.com>
2023-11-21 17:06:20 +0100

    added docker to github repository

---
+++ merge_jupyter_lab_wDocker/RtoBeInstalled/listForDockerfileR.sh
@@ -0,0 +1,22 @@
+R CMD INSTALL --build \
+/tmp/packages/fastmap_1.1.1.tar.gz \
+/tmp/packages/rlang_1.1.2.tar.gz \
+/tmp/packages/cli_3.6.1.tar.gz \
+/tmp/packages/fansi_1.0.5.tar.gz \
+/tmp/packages/glue_1.6.2.tar.gz \
+/tmp/packages/utf8_1.2.4.tar.gz \
+/tmp/packages/base64enc_0.1-3.tar.gz \
+/tmp/packages/evaluate_0.23.tar.gz \
+/tmp/packages/pbdZMQ_0.3-10.tar.gz \
+/tmp/packages/crayon_1.5.2.tar.gz \
+/tmp/packages/jsonlite_1.8.7.tar.gz \
+/tmp/packages/uuid_1.1-1.tar.gz \
+/tmp/packages/digest_0.6.33.tar.gz \
+/tmp/packages/ellipsis_0.3.2.tar.gz \
+/tmp/packages/lifecycle_1.0.4.tar.gz \
+/tmp/packages/vctrs_0.6.4.tar.gz \
+/tmp/packages/htmltools_0.5.7.tar.gz \
+/tmp/packages/pillar_1.9.0.tar.gz \
+/tmp/packages/repr_1.1.6.tar.gz \
+/tmp/packages/IRdisplay_1.1.tar.gz \
+/tmp/packages/IRkernel_1.3.2.tar.gz \
